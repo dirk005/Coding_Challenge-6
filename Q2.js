2e1708c34335bc62488cc78ec430eb80064afbf9
Dirk <dirkvanrensburg5@gmail.com>
2018-06-13 13:35:21 +0200

    adding question 2

---
+++ Q2.js
@@ -0,0 +1,18 @@
+const answer = (arr,tar) => {
+  answerArr =[];
+   for (let num of arr) {
+      for(let num2 of arr){
+         if( num + num2 === tar && num !== num2 ){
+          answerArr = [num,num2];
+          return answerArr;
+        }        
+     }    
+  }
+ return answerArr;
+}
+
+const array = [1,2,5];
+const tar = 4;
+answer(array,tar)
+
+answerArr.length ? console.log(answerArr) : console.log(`No values in array [${array}] adds to ${tar}`);
